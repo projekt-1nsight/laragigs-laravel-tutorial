19b6f90b2124430aeaf878a305dd9a122ea0718f
1NSiGHT <kent.olayvar117@gmail.com>
2023-03-13 09:59:45 -0300

    Published Tutorial site

---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+
+Common naming conventions in Laravel - (Notes taken from TraversyMedia Laravel Course)
+Common Resource Routes:
+    index - Show all listings
+    show - Show single listing
+    create - Show form to create new listing
+    store - Store new listing
+    edit - Show form to edit listing
+    update - Update listing
+    destroy - Delete Listing
+|
+*/
+
+
+//All Listings
+Route::get('/', [ListingController::class, 'index']);
+
+//Show create form
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+//Store Listings data
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+
+//Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+//Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+
+//Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+//Manage Listings
+Route::get('listings/manage', [ListingController::class, 'manage'])->middleware('auth');
+
+//Single Listings
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//Show Register/Create Form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+//Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+//Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+//Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+//Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+
